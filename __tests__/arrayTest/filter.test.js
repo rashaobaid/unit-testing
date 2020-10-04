09673a3635591613e37d7ee3c06b21e31f565a40
Rasha Obaid <rasha-rashad1997@hotmail.com>
2020-10-04 16:07:15 +0300

    addTestKeys in filter.test.js

--- __tests__/arrayTest/filter.test.js
+++ __tests__/arrayTest/filter.test.js
@@ -1,7 +1,16 @@
 const filter =require('../../arrayMethod/filter');
 
 test('should ', () => {
-    let myData =[1,5,10,11,8,9,12];
-    expect(filter(myData, 2)).toEqual([5,10,11,8,9,12]);
+    let myData =[1,5,12,0];
+    expect(filter(myData, x=> x>2)).toEqual([5,12]);
+    expect(filter()).toEqual([]);
+    expect(filter(myData)).toEqual([]);
+    expect(filter([])).toEqual([]);
+    expect(filter([],x=> x>2)).toEqual([]);
+    expect(filter({})).toEqual([]);
+    expect(filter({},x=> x>2)).toEqual([]);
+    expect(filter({a:"123",b:"1",c:"5"},x=> x>2)).toEqual([{a:"123",c:"5"}])
+    expect(filter({"14124":"123", [124]:"1234",c:"5"},x=> x==5)).toEqual([{c:"5"}])
+    expect(filter({"14124":"1", [124]:"30"},x=> x>3)).toEqual([{[124]:"30"}])
     
 })
