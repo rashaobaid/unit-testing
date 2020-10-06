220e223994e90e1c3325ef95e127d05c8350686a
Rasha Obaid <rasha-rashad1997@hotmail.com>
2020-10-06 17:19:38 +0300

    update entries

--- objectMethod/entries.js
+++ objectMethod/entries.js
@@ -1,15 +1,10 @@
 function entries(obj){
-    if(Array.isArray(obj)){
-        let result =[...new Map(Object.values(obj).map((e, idx) => ([idx, e])))];
-       return result.sort();
-    }
         let arr = [];
         for (var key in obj) {
             arr.push([key, obj[key]]);
         }
-        return arr.sort();
+        return arr;
 
-    
 }
 
 module.exports = entries
